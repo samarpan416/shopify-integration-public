290d7eb8c0ee12d0304d8e7e39f20ba2cf3e0208
Ashish Mittal <ashishmittal@Ashishs-MacBook-Pro.local>
2022-04-28 17:29:24 +0530

    Initial Setup

---
+++ src/test/java/com/uniware/integrations/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniware.integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
