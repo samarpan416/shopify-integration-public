56a1fe84898db3984218cc9c79ad35f937113d33
Samarpan Harit <samarpan.harit@unicommerce.com>
2022-10-10 14:27:10 +0530

    Added sale order customisations for oziva (#14)

--- src/main/java/com/uniware/integrations/services/saleorder/impl/OzivaSaleOrderService.java
+++ src/main/java/com/uniware/integrations/services/saleorder/impl/OzivaSaleOrderService.java
@@ -1,6 +1,12 @@
 package com.uniware.integrations.services.saleorder.impl;
 
+import com.unifier.core.utils.StringUtils;
 import com.uniware.integrations.clients.ShopifyClient;
+import com.uniware.integrations.dto.ConfigurationParameters;
+import com.uniware.integrations.dto.ConnectorParameters;
+import com.uniware.integrations.dto.shopify.Order;
+import com.uniware.integrations.utils.ShopifyUtils;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +16,27 @@ public class OzivaSaleOrderService extends BaseSaleOrderService {
 
     public OzivaSaleOrderService(ShopifyClient shopifyClient) {
         super(shopifyClient);
+    }
+
+    @Override
+    public boolean shouldFetchOrder(Order order, ConfigurationParameters configurationParameters,
+            ConnectorParameters connectorParameters) {
+        boolean shouldFetchOrder = super.shouldFetchOrder(order, configurationParameters, connectorParameters);
+        if(shouldFetchOrder) {
+            String paymentMode = getPaymentMode(order);
+            if("cod".equals(paymentMode) && !StringUtils.equalsIngoreCaseAny(order.getTags(), "COD_CONFIRM", "COD_CONFIRM_SPECIAL", "COD_CONFIRM_MANUAL")) {
+                shouldFetchOrder = false;
+            }
+        }
+        return shouldFetchOrder;
+    }
+
+    @Override
+    public String getPaymentMode(Order order) {
+        String paymentMode = super.getPaymentMode(order);
+        if (ShopifyUtils.containsAnyIgnoreCase(order.getTags(), "COD_CONFIRM_SPECIAL", "COD_CONFIRM")) {
+            paymentMode = "cod";
+        }
+        return paymentMode;
     }
 }
